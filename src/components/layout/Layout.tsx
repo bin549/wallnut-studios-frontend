f0d4ffb78105073d67119cbdc11b8efaaa7dbbce
bin892 <549288160@qq.com>
2023-09-17 22:52:10 +0800

    reworing.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,40 +1,39 @@
-import { Fragment } from 'react';
-import { useEffect } from 'react';
+import {Fragment} from 'react';
+import {useEffect} from 'react';
+import React from 'react'
 
 import classes from './Layout.module.css';
 import MainNavigation from './MainNavigation';
 import Figure from 'react-bootstrap/Figure';
 import useHttp from '../../hooks/use-http';
-import { getAllQuotes } from '../../lib/api';
+import {getAllQuotes} from '../../lib/api';
+import {Button, Form, Grid, Header, Image, Message, Segment} from 'semantic-ui-react'
 
 
 const Layout = (props: any) => {
-  const { sendRequest, status, data: loadedQuotes, error } = useHttp(
-    getAllQuotes,
-    true
-  );
+    const {sendRequest, status, data: loadedQuotes, error} = useHttp(
+        getAllQuotes,
+        true
+    );
 
-  useEffect(() => {
-    sendRequest();
-  }, [sendRequest]);
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
 
-  return (
-    <Fragment>
-      <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
-      <Figure>
-          <Figure.Image
-            width={171}
-            height={180}
-            alt="171x180"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN7ZJW4kUXy9hjRk9eOpgLtv7glLvghcACX3d4GBYlvw&s"
-          />
-          <Figure.Caption>
-            img.
-          </Figure.Caption>
-        </Figure>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <MainNavigation/>
+            <main className={classes.main}>{props.children}</main>
+            {/*<Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>*/}
+            {/*    <Grid.Column style={{maxWidth: 450}}>*/}
+            {/*        <Header as='h2' color='teal' textAlign='center'>*/}
+            {/*            <Image*/}
+            {/*                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN7ZJW4kUXy9hjRk9eOpgLtv7glLvghcACX3d4GBYlvw&s'/>*/}
+            {/*        </Header>*/}
+            {/*    </Grid.Column>*/}
+            {/*</Grid>*/}
+        </Fragment>
+    );
 };
 
 export default Layout;
