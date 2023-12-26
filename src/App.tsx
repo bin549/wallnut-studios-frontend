8518bfa313dade5826c2817f7d1e80d395c414f4
--# <--#@gmail.com>
2023-12-26 17:17:54 +0800

    feat: add my skill information.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,112 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  return (
-    <Layout>
-
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div>
+                skill(sort by current workflow):
+                <div>
+                    Programming Language:
+                    <br/>
+                    Python,
+                    C++,
+                    Go,
+                    Java,
+                    Rust,
+                    Typescript,
+                    C#,
+                    Dart,
+                    Swift,
+                    GdScript
+                </div>
+                <br/>
+                <div>
+                    Game Engine:
+                    <br/>
+                    Godot, Unity, Bevy, Pygame
+                </div>
+                <br/>
+                <div>
+                    Backend Framework:
+                    <br/>
+                    Django, FastAPI, SpringWeb, Flask
+                </div>
+                <br/>
+                <div>
+                    Web Framework:
+                    <br/>
+                    React, Vue, Svelte
+                </div>
+                <br/>
+                <div>
+                    Mobile Framework:
+                    <br/>
+                    Flutter, Android Studios, Swift UI
+                </div>
+                <br/>
+                <div>
+                    Desktop Framework:
+                    <br/>
+                    Qt
+                </div>
+                <br/>
+                <div>
+                    Instrument:
+                    <br/>
+                    Account Guitar, Piano, Elestric Guitar, Ukulele
+                </div>
+                <br/>
+                <div>
+                    Video:
+                    <br/>
+                    Pr
+                </div>
+                <br/>
+                <div>
+                    Image:
+                    <br/>
+                    Ps, Asprite, Blender
+                </div>
+                <br/>
+                <div>
+                    DB:
+                    <br/>
+                    Postgres, Mysql
+                </div>
+                <br/>
+                <div>
+                    Instrument Software:
+                    <br/>
+                    FL Studios, Guitar Pro, Audacity
+                </div>
+                <br/>
+                <div>
+                    Tool:
+                    <br/>
+                    Docker, Jenkins, GraphQL
+                </div>
+                <br/>
+                <div>
+                    Web:
+                    <br/>
+                    Unocss, tailwind, AlphineJS
+                </div>
+                <br/>
+                <div>
+                    Service:
+                    <br/>
+                    Nginx, Tomcat
+                </div>
+                <br/>
+                <div>
+                    Spider:
+                    <br/>
+                    Scrapy
+                </div>
+                <br/>
+            </div>
+        </Layout>
+    );
 }
 
 export default App;
--- src/components/layout/MainNavigation.tsx
+++ src/components/layout/MainNavigation.tsx
@@ -1,15 +1,16 @@
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
-  return (
-    <header className={classes.header}>
-      <div className={classes.logo}>肩部检测在线网址</div>
-      <nav className={classes.nav}>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>Wallnut Studios</div>
+            <nav className={classes.nav}>
+                <a href={"https://github.com/bin549"}>🐰github-Link</a>
+            </nav>
+        </header>
+    );
 };
 
 export default MainNavigation;
