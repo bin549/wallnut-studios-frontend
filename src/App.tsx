52a6b3d33e9a5bd6b679ca2967bad02bd96d5a3f
--# <--#@gmail.com>
2023-12-27 13:44:49 +0800

    feat: restyle profile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,143 @@
 import Layout from './components/layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from './components/App.module.css';
 
 
 function App() {
     return (
         <Layout>
             <div>
-                skill(sort by current workflow):
+                <h1 className={classes.header}>My Skill</h1>
                 <div>
-                    Programming Language:
-                    <br/>
-                    Python,
-                    C++,
-                    Go,
-                    Java,
-                    Rust,
-                    Typescript,
-                    C#,
-                    Dart,
-                    Swift,
-                    kotlin,
-                    GdScript
+                    <h5 className={classes.title}>Programming Language</h5>
+                    <div className={classes.content}>
+                        Python,
+                        GDScript
+                        C++,
+                        Go,
+                        Java,
+                        Rust,
+                        Typescript,
+                        C#,
+                        Dart,
+                        Swift,
+                        kotlin
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Game Engine:
-                    <br/>
-                    Godot, Unity, Bevy, Pygame
+                    <h5 className={classes.title}>Game Engine</h5>
+                    <div className={classes.content}>
+                        Godot, Unity, Bevy, Pygame
+                    </div>
+
                 </div>
                 <br/>
                 <div>
-                    Backend Framework:
-                    <br/>
-                    Django, FastAPI, SpringWeb, Gin, Flask
+                    <h5 className={classes.title}>Backend Framework</h5>
+                    <div className={classes.content}>
+                        Django, FastAPI, SpringWeb, Gin, Flask
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Web Framework:
-                    <br/>
-                    React, Vue, Svelte
+                    <h5 className={classes.title}>Web Framework</h5>
+                    <div className={classes.content}>
+                        React, Vue, Svelte
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Mobile Framework:
-                    <br/>
-                    Flutter, Android Studios, Swift UI
+                    <h5 className={classes.title}>Mobile Framework</h5>
+                    <div className={classes.content}>
+                        Flutter, Android Studios, Swift UI
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Desktop Framework:
-                    <br/>
-                    Qt
+                    <h5 className={classes.title}>Desktop Framework</h5>
+                    <div className={classes.content}> Qt
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Instrument:
-                    <br/>
-                    Account Guitar, Piano, Elestric Guitar, Ukulele
+                    <h5 className={classes.title}>Instrument</h5>
+                    <div className={classes.content}> Account Guitar, Piano, Elestric Guitar, Ukulele
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Video:
-                    <br/>
-                    Pr, Kdenlive
+                    <h5 className={classes.title}>Video</h5>
+                    <div className={classes.content}> Pr, Kdenlive
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Image:
-                    <br/>
-                    Ps, Asprite, Blender
+                    <h5 className={classes.title}>Image</h5>
+                    <div className={classes.content}> Ps, Asprite, Pyxel Pixelorama, Blender
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    DB:
-                    <br/>
-                    Postgres, Mysql
+                    <h5 className={classes.title}>DB</h5>
+                    <div className={classes.content}> Postgres, Mysql
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Instrument Software:
-                    <br/>
-                    FL Studios, Guitar Pro, Audacity
+                    <h5 className={classes.title}>Instrument Software</h5>
+                    <div className={classes.content}> FL Studios, Guitar Pro, Audacity
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Tool:
-                    <br/>
-                    Docker, Jenkins, GraphQL
+                    <h5 className={classes.title}>Tool</h5>
+                    <div className={classes.content}> Docker, Jenkins, GraphQL
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Web:
-                    <br/>
-                    Unocss, tailwind, AlphineJS
+                    <h5 className={classes.title}>Design</h5>
+                    <div className={classes.content}> Figma
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Service:
-                    <br/>
-                    Nginx, Tomcat
+                    <h5 className={classes.title}>Web</h5>
+                    <div className={classes.content}> Unocss, Tailwind, AlphineJS
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Web Spider Tool:
-                    <br/>
-                    Scrapy, Selenium, Requests
+                    <h5 className={classes.title}>Service</h5>
+                    <div className={classes.content}> Nginx, Tomcat
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Things I like:
-                    <br/>
-                    Game, Book, Movie, Music
+                    <h5 className={classes.title}>Web Crawler</h5>
+                    <div className={classes.content}> Scrapy, Selenium, Requests
+                    </div>
                 </div>
                 <br/>
                 <div>
-                    Sport I like:
-                    <br/>
-                    Run
+                    <h5 className={classes.title}>Things I like</h5>
+                    <div className={classes.content}> Game, Book, Movie, Music
+                    </div>
+                </div>
+                <br/>
+                <div>
+                    <h5 className={classes.title}>Sport I like</h5>
+                    <div className={classes.content}>
+                        Run
+                    </div>
+                </div>
+                <br/>
+                <div>
+                    <h5 className={classes.title}>My Name</h5>
+                    <div className={classes.content}>
+                        LWBZ
+                    </div>
                 </div>
                 <br/>
             </div>
