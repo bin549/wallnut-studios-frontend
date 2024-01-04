330a7e05f024497c504895c1c9b1e17accb02295
--# <--#@gmail.com>
2024-01-04 15:46:20 +0800

    chore: dynamic data & remove useless files.

--- src/App.tsx
+++ src/App.tsx
@@ -2,155 +2,184 @@ import Layout from './components/layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import classes from './components/App.module.css';
 
+const skill_sets = [
+    {
+        tag: "Programming Language",
+        skills: [
+            "Python",
+            "GDScript",
+            "Typescript",
+            "Dart",
+            "Go",
+            "Rust",
+            "C++",
+            "C#",
+            "Java"
+        ]
+    },
+    {
+        tag: "Backend Framework",
+        skills: [
+            "Godot",
+            "Unity"
+        ]
+    },
+    {
+        tag: "Web Framework",
+        skills: [
+            "Django",
+            "FastAPI",
+            "Gin",
+            "SpringWeb"
+        ]
+    },
+    {
+        tag: "Mobile Framework",
+        skills: [
+            "Flutter"
+        ]
+    },
+    {
+        tag: "Desktop Framework",
+        skills: [
+            "Qt"
+        ]
+    },
+    {
+        tag: "Database",
+        skills: [
+            "Postgres",
+            "Mysql"
+        ]
+    },
+    {
+        tag: "Video Software",
+        skills: [
+            "Premiere Pro",
+            "Kdenlive"
+        ]
+    },
+    {
+        tag: "2D Art Software",
+        skills: [
+            "Photoshop",
+            "Asprite",
+            "Pyxel",
+            "Pixelorama"
+        ]
+    },
+    {
+        tag: "3D Art Software",
+        skills: [
+            "Blender",
+            "Material Maker"
+        ]
+    },
+    {
+        tag: "Audio Software",
+        skills: [
+            "FL Studios",
+            "Audacity",
+            "Guitar Pro"
+        ]
+    },
+    {
+        tag: "Dev Tool",
+        skills: [
+            "Docker",
+            "Jenkins",
+            "GraphQL"
+        ]
+    },
+    {
+        tag: "Design Tool",
+        skills: [
+            "Figma"
+        ]
+    },
+    {
+        tag: "Service Tool",
+        skills: [
+            "Nginx",
+            "Tomcat"
+        ]
+    },
+    {
+        tag: "Test Framework",
+        skills: [
+            "Selenium"
+        ]
+    },
+    {
+        tag: "Network Framework",
+        skills: [
+            "Requests"
+        ]
+    },
+    {
+        tag: "Crawler Technique",
+        skills: [
+            "Scrapy",
+            "Urllib",
+            "XPath"
+        ]
+    },
+    {
+        tag: "Instrument",
+        skills: [
+            "Account Guitar",
+            "Piano",
+            "Elestric Guitar",
+            "Ukulele"
+        ]
+    },
+    {
+        tag: "Things I like",
+        skills: [
+            "Game",
+            "Music",
+            "Book",
+            "Movie",
+            "Anim"
+        ]
+    },
+    {
+        tag: "Sport I like",
+        skills: [
+            "Run"
+        ]
+    },
+    {
+        tag: "My Name",
+        skills: [
+            "LWBZ"
+        ]
+    },
+
+]
+
+function SkillRow(props: any) {
+    return (
+        <div>
+            <h5 className={classes.title}>{props.tag}</h5>
+            <div className={classes.content}>
+                {props.skills.toString()}
+            </div>
+            <br/>
+        </div>
+    );
+}
 
 function App() {
     return (
         <Layout>
             <h1 className={classes.header}>My Skill</h1>
             <div className={classes.body}>
-                <div>
-                    <h5 className={classes.title}>Programming Language</h5>
-                    <div className={classes.content}>
-                        Python,
-                        GDScript
-                        C++,
-                        Go,
-                        Java,
-                        Rust,
-                        Typescript,
-                        C#,
-                        Dart,
-                        Swift,
-                        kotlin
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Game Engine</h5>
-                    <div className={classes.content}>
-                        Godot, Unity, Bevy, Pygame
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Backend Framework</h5>
-                    <div className={classes.content}>
-                        Django, FastAPI, SpringWeb, Gin, Flask
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Web Framework</h5>
-                    <div className={classes.content}>
-                        React, Vue, Svelte
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Mobile Framework</h5>
-                    <div className={classes.content}>
-                        Flutter, Android Studios, Swift UI
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Desktop Framework</h5>
-                    <div className={classes.content}> Qt, .NET, PyQt
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Database</h5>
-                    <div className={classes.content}> Postgres, Mysql
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Video Software</h5>
-                    <div className={classes.content}> Premiere Pro, Kdenlive
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>2D Art Software</h5>
-                    <div className={classes.content}> Photoshop, Asprite, Pyxel Pixelorama
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>3D Art Software</h5>
-                    <div className={classes.content}> Blender, Material Maker
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Audio Software</h5>
-                    <div className={classes.content}> FL Studios, Guitar Pro, Audacity
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Dev Tool</h5>
-                    <div className={classes.content}> Docker, Jenkins, GraphQL
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Design Tool</h5>
-                    <div className={classes.content}> Figma
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Service</h5>
-                    <div className={classes.content}> Nginx, Tomcat
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Test Framework</h5>
-                    <div className={classes.content}> Selenium
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Network Framework</h5>
-                    <div className={classes.content}> Requests
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Crawler Technique</h5>
-                    <div className={classes.content}> Scrapy, Urllib, XPath
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Instrument</h5>
-                    <div className={classes.content}> Account Guitar, Piano, Elestric Guitar, Ukulele
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Things I like</h5>
-                    <div className={classes.content}> Game, Music, Book, Movie, Anim
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>Sport I like</h5>
-                    <div className={classes.content}>
-                        Run
-                    </div>
-                </div>
-                <br/>
-                <div>
-                    <h5 className={classes.title}>My Name</h5>
-                    <div className={classes.content}>
-                        LWBZ
-                    </div>
-                </div>
-                <br/>
+                {
+                    skill_sets.map((skill_set) => (
+                        <SkillRow key={skill_set.tag} tag={skill_set.tag} skills={skill_set.skills}/>
+                    ))
+                }
             </div>
         </Layout>
     );
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,3 @@
-const FIREBASE_DOMAIN = '127.0.0.1:5000';
-
 
 export async function getAllQuotes() {
 	const response = await fetch('http://127.0.0.1:5000/api/orders/orders/', {
