3ef997354b2edbabf4b3cb13337e4b816ec6dbf3
--# <--#@gmail.com>
2024-01-15 16:47:47 +0800

    chore: beautify first page.

--- src/pages/SubModule.tsx
+++ src/pages/SubModule.tsx
@@ -6,17 +6,18 @@ const SubModule = () => {
         <div className={classes.layout}>
             {
                 SUBMODULES.map((module, index) => (
-                    <div className={index % 2 === 0 ? classes.module_item : classes.module_item_reverse}>
-                        <div className={classes.module_item_header}>
-                            <img src={module.image}/>
-                        </div>
-                        <div className={classes.module_item_body}>
-                            <h4>{module.title}</h4>
-                            <span>
+                        <div className={index % 2 === 0 ? classes.module_item : classes.module_item_reverse}>
+                            <div className={classes.module_item_header}>
+                                <img src={module.image}/>
+                            </div>
+                            <div className={classes.module_item_body}>
+                                <h4>{module.title}</h4>
+                                <span>
                             {module.description}
                             </span>
+                            </div>
                         </div>
-                    </div>
+                    )
                 )
             }
         </div>
