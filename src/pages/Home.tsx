885fc0e7fc7c224de9168417ad45c1c16872e319
--# <--#@gmail.com>
2024-02-05 14:43:26 +0800

    chore: get skill api.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,10 @@ const Home = () => {
             method: "GET",
             credentials: "include",
         }
-        fetch(`http://127.0.0.1:5000`, requestOptions)
+        fetch(`http://127.0.0.1:5000/api/skill/list`, requestOptions)
+            .then((res) => {
+                console.log(res.json())
+            })
     })
 
     return (
