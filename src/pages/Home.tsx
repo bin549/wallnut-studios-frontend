4114b0359c1d9ecb3b75a6ec1d00532c1d99b92f
--# <--#@gmail.com>
2024-02-05 16:35:53 +0800

    chore: improve skill api.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,17 +11,6 @@ const Home = () => {
         setIsCheck(!isCheck)
     }
 
-    useEffect(() => {
-        const requestOptions : any = {
-            method: "GET",
-            credentials: "include",
-        }
-        fetch(`http://127.0.0.1:5000/api/skill/list`, requestOptions)
-            .then((res) => {
-                console.log(res.json())
-            })
-    })
-
     return (
         <Layout onProfileCheck={toggleIsCheck}>
             {!isCheck && <SubModule/>}
--- src/pages/Skill.tsx
+++ src/pages/Skill.tsx
@@ -1,15 +1,40 @@
 import classes from "./Skill.module.css";
 import {SKELLs} from "../data";
 import SkillRow from "../components/SkillRow";
+import {useEffect, useState} from "react"
 
 const Skill = () => {
+    const [skills, setSkills] = useState([])
+
+    useEffect(() => {
+        const requestOptions: any = {
+            method: "GET",
+            credentials: "include",
+        }
+        const skill_temp: any[] = []
+        fetch(`http://127.0.0.1:5000/api/skill/list`, requestOptions)
+            .then((res: any) =>
+                res.json()
+            )
+            .then((data: any) => {
+                Array.from(new Set(data.map((skill: any) => skill.tag))).forEach(skill_tag => {
+                    skill_temp.push({
+                        tag: skill_tag,
+                        skills:Array.from(data.filter((skill:any) => skill.tag === skill_tag).map((skill:any) => skill.name))
+                    })
+                })
+                // @ts-ignore
+                setSkills(skill_temp)
+            })
+    })
+
     return (
         <>
             <div className={classes.layout}>
                 <h1 className={classes.header}>My Skill</h1>
                 <div className={classes.body}>
                     {
-                        SKELLs.map((skill_set: any) => (
+                        skills.map((skill_set: any) => (
                             <SkillRow key={skill_set.tag} tag={skill_set.tag} skills={skill_set.skills}/>
                         ))
                     }
