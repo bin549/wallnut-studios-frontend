8766df453634798ecc8fad2ae7d7d786a55dec46
--# <--#@gmail.com>
2024-02-09 14:54:45 +0800

    feat: add avatar.

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,10 +1,20 @@
+import Avatar from "../assets/avatar.png"
+import classes from "./AboutMe.module.css"
 
 const AboutMe = () => {
 
 
     return (
         <>
-            About Me
+            <div className={classes.avatar_card}>
+                <img src={Avatar}/>
+                <div>
+                    我是Z
+                </div>
+                <p>
+                    我来自汕头，金牛男，infj
+                </p>
+            </div>
         </>
     )
 }
